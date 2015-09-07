4402c33997d0c5d14842dc34cbfcf3bfe26114f0
linkelvin11 <lin.kelvin11@gmail.com>
2015-09-07 19:53:57 -0400

    ps1 basic functionality done

--- PS1_System_Calls__Error_Checking_and_Reporting/PS1.c
+++ PS1_System_Calls__Error_Checking_and_Reporting/PS1.c
@@ -1,15 +1,20 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 int main (int argc, char **argv)
 {
+	if (argc == 1) // if there are no arguments, exit.
+		return 0;
+	int buff_def = 1024	;	// default buffer size
 	int outfile = 1; 		// default to stdout
 	int infiles[100]; 		// default to stdin
-	int buffersize = 262144;// default to 256k
+	int buffersize = buff_def;
 	int maxflags = 5<argc?5:argc;
 
 	// Check for flags
@@ -20,22 +25,24 @@ int main (int argc, char **argv)
 		if (strcmp(argv[i],"-b") == 0) // check for buffer size flag
 		{
 			i++;
-			if (buffersize == 262144) // check if buffersize has already been initialized
+			if (buffersize == buff_def) // check if buffersize has already been initialized
 			{
-				if (i < maxflags)
+				if (i < maxflags ) // check that there are enough input arguments left
 				{
 					j = i + 1;
 					buffersize = atoi(argv[i]);
+					if (buffersize == 0)
+						perror("ERROR: Entered an invalid buffer size. \n");
 				}
 				else
 				{
-					printf("ERROR: Missing buffersize argument. \n");
+					perror("ERROR: Missing buffersize argument. \n");
 					return -1;
 				}
 			}
 			else
 			{
-				printf("ERROR: Attempted to reinitialize buffer; Buffer has already been initialized. \n");
+				perror("ERROR: Attempted to reinitialize buffer; Buffer has already been initialized. \n");
 				return -1; // placeholder -- report an error about buffer size already initialized
 			}		
 		}
@@ -44,37 +51,60 @@ int main (int argc, char **argv)
 			i++;
 			if (outfile == 1) // check if outfile has already been opened
 			{
-				if (i < maxflags)
+				if (i < maxflags) // check that there are enough input arguments left
 				{
 					j = i + 1;
-					outfile = open(argv[i],O_WRONLY | O_CREAT);
+					outfile = open(argv[i],O_WRONLY | O_CREAT, 0666);
 				}
 				else
 				{
-					printf("ERROR: Missing outfile argument. \n");
+					perror("ERROR: Missing outfile argument. \n");
 					return -1;
 				}
 			}
 			else
 			{
-				printf("ERROR: Attempted to open more than one output file. \n");
+				perror("ERROR: Attempted to open more than one output file. \n");
 				return -1;
 			}
 		}
 	}
 	printf("%d %d\n",i,j);
 
-	int offset = j;
 	// open input files
+	int offset = j;
+	char* buf;
+	if ((buf = malloc(buffersize)) == 0)
+	{
+		fprintf(stderr, "ERROR: Could not allocate %d bytes of memory to buffer with malloc: %s\n", 
+				buffersize, strerror(errno));
+		return -1;
+	}
+	int rd = 0;
+	int wr = 0;
 
 	for (; j < argc; j++)
 	{
 		if (strcmp(argv[j],"-") != 0)
-			infiles[j-offset] = open(argv[j],O_RDONLY);
+			infiles[j-offset] = open(argv[j],O_RDONLY,0666); // remember to check for errors here
+		while((rd = read(infiles[j-offset],buf,buffersize)) != 0)
+		{
+			if (rd < 0)
+			{
+				perror("ERROR: Error while reading input");
+				free(buf);
+				return -1;
+			}
+			printf("rd = %d\n",rd);
+			wr = write(outfile,buf,rd);
+			
+			if (wr < 0)
+			{
+				perror("ERROR: Unable to write to output");
+				free(buf);
+				return -1;
+			}
+		}
 	}
-
-
-
-
 	return 0; // no errors were encountered
 }
