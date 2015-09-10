c75125809258857b14b0d801c594305f11429c95
linkelvin11 <lin.kelvin11@gmail.com>
2015-09-09 22:49:42 -0400

    ps1 added partial-write contingency and file close error handling

--- PS1_System_Calls__Error_Checking_and_Reporting/PS1.c
+++ PS1_System_Calls__Error_Checking_and_Reporting/PS1.c
@@ -7,17 +7,19 @@
 #include <string.h>
 #include <errno.h>
 
+#define BUFF_DEFAULT 1024	// default buffer size
+
 int main (int argc, char **argv)
 {
-	if (argc == 1) // if there are no arguments, exit.
-		return 0;
-	int buff_def = 1024	;	// default buffer size
+	if (argc == 1) 			// if there are no arguments, exit.
+		return 0;	
 	int outfile = 1; 		// default to stdout
 	int infiles[100]; 		// default to stdin
-	int buffersize = buff_def;
+	int buffersize = BUFF_DEFAULT;
 	int maxflags = 5<argc?5:argc;
 
 	// Check for flags
+	// consider using getopt (man 3 getopt)
 	int i = 1;
 	int j = 1;
 	for (; i < maxflags; i++) // only need to check up to first 4 arguments for flags
@@ -25,7 +27,7 @@ int main (int argc, char **argv)
 		if (strcmp(argv[i],"-b") == 0) // check for buffer size flag
 		{
 			i++;
-			if (buffersize == buff_def) // check if buffersize has already been initialized
+			if (buffersize == BUFF_DEFAULT) // check if buffersize has already been initialized
 			{
 				if (i < maxflags ) // check that there are enough input arguments left
 				{
@@ -76,19 +78,24 @@ int main (int argc, char **argv)
 	char* buf;
 	if ((buf = malloc(buffersize)) == 0)
 	{
-		fprintf(stderr, "ERROR: Could not allocate %d bytes of memory to buffer with malloc: %s\n", 
+		fprintf(stderr, 
+				"ERROR: Could not allocate %d bytes of memory to buffer with malloc: %s\n", 
 				buffersize, strerror(errno));
 		return -1;
 	}
-	int rd = 0;
-	int wr = 0;
+	int rd = 0;		// bytes read
+	int wr = 0;		// bytes written
+	int wlen = 0;	// progress in buffer
 
 	for (; j < argc; j++)
 	{
+		// open the file input from argv
 		if (strcmp(argv[j],"-") != 0)
 			infiles[j-offset] = open(argv[j],O_RDONLY,0666); // remember to check for errors here
 		else
 			infiles[j-offset] = 0;
+
+		// read from file input and write to ouput
 		while((rd = read(infiles[j-offset],buf,buffersize)) != 0)
 		{
 			if (rd < 0)
@@ -97,16 +104,46 @@ int main (int argc, char **argv)
 				free(buf);
 				return -1;
 			}
-			printf("rd = %d\n",rd);
-			wr = write(outfile,buf,rd);
-			
-			if (wr < 0)
+			//printf("rd = %d\n",rd);
+
+			// Check for partial writes
+			wlen = 0;
+			do
 			{
-				perror("ERROR: Unable to write to output");
-				free(buf);
-				return -1;
+				wr = write(outfile,buf+wlen,rd-wlen);
+				wlen = wlen + wr;
+				if (wr < 0)
+				{
+					perror("ERROR: Unable to write to output");
+					free(buf);
+					return -1;
+				}	
 			}
+			while(wlen < rd);
+			
+		}
+
+		// close the file if its not stdin
+		if (infiles[j-offset] != 0 &&
+			close(infiles[j-offset]) < 0)
+		{
+			perror("ERROR: There was an error closing an input file");
+			free(buf);
+			return -1;
 		}
 	}
-	return 0; // no errors were encountered
+
+	free(buf);
+
+	// close output file if its not stdout
+	if (outfile != 1 &&
+		close(outfile) < 0)
+	{
+		perror("ERROR: There was an error closing the output file");
+		return -1;
+	}
+
+	// no errors were encountered
+	// exit program
+	return 0; 
 }
