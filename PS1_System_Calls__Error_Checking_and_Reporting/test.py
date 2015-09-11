832fd6ef4ab5d0da533f36bc45d5538c319224cf
linkelvin11 <lin.kelvin11@gmail.com>
2015-09-10 20:04:25 -0400

    added buffersize metrics and input files

--- PS1_System_Calls__Error_Checking_and_Reporting/test.py
+++ PS1_System_Calls__Error_Checking_and_Reporting/test.py
@@ -1,2 +1,13 @@
 # Test Script for copycat
 
+import os
+import math
+
+cmd = ''
+print(str(2^2))
+for i in range(0, 19):
+	buffersize = str(pow(2,i))
+	cmd = "time ./copycat -b " + buffersize + " -o xout.txt xa*"
+	#print(cmd)
+	print("buffersize = " + buffersize)
+	os.system(cmd)
