39c41427431b2361c8d00ec54e29835eee735107
linkelvin11 <lin.kelvin11@gmail.com>
2015-09-11 00:27:31 -0400

    ps1 more test cases for test.py

--- PS1_System_Calls__Error_Checking_and_Reporting/test.py
+++ PS1_System_Calls__Error_Checking_and_Reporting/test.py
@@ -3,11 +3,29 @@
 import os
 import math
 
-cmd = ''
-print(str(2^2))
+def command(com):
+	print(com)
+	os.system(com)
+
+command("make clean")
+command("make")
+command("split -n 10 lipsum.txt")
+print("---test invalid buffer sizes---")
+command("./copycat -b buffersize -o xout.txt lipsum.txt")
+print("---test multiple buffers---")
+command("./copycat -b 10 -b 50 -o xout.txt lipsum.txt")
+print("---test invalid output files---")
+command("chmod 000 xout.txt")
+command("./copycat -o xout.txt lipsum.txt")
+command("chmod 777 xout.txt")
+print("---test invalid input files---")
+command("./copycat -o xout.txt nonexistent file")
+print("---test multiple file concatenation---")
+command("split -n 10 lipsum.txt")
+command("./copycat -o xout.txt xa*")
+command("diff xout.txt lipsum.txt")
+print("---test speed for multiple buffer sizes---")
 for i in range(0, 19):
 	buffersize = str(pow(2,i))
-	cmd = "time ./copycat -b " + buffersize + " -o xout.txt xa*"
-	#print(cmd)
-	print("buffersize = " + buffersize)
-	os.system(cmd)
+	print("buffersize = " + str(buffersize) + " bytes")
+	os.system("/usr/bin/time -f \"%E real,\t%U user,\t%S sys\" ./copycat -b " + buffersize + " -o xout.txt xa*")
