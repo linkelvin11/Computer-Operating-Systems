a0d9dc5b25e2832dbfc4f1cc8b5b9b91788d95cb
linkelvin11 <lin.kelvin11@gmail.com>
2015-09-11 00:46:07 -0400

    ps1 added more test cases

--- PS1_System_Calls__Error_Checking_and_Reporting/test.py
+++ PS1_System_Calls__Error_Checking_and_Reporting/test.py
@@ -7,24 +7,40 @@ def command(com):
 	print(com)
 	os.system(com)
 
-command("make clean")
-command("make")
-command("split -n 10 lipsum.txt")
-print("---test invalid buffer sizes---")
+# generate test files
+os.system("make clean")
+os.system("make")
+os.system("split -n 10 lipsum.txt")
+
+print("\n---test unspecified buffersize---")
+command("./copycat -b")
+
+print("\n---test invalid buffer sizes---")
 command("./copycat -b buffersize -o xout.txt lipsum.txt")
-print("---test multiple buffers---")
+
+print("\n---test multiple buffers---")
 command("./copycat -b 10 -b 50 -o xout.txt lipsum.txt")
-print("---test invalid output files---")
+
+print("\n---test unspecified output file---")
+command("./copycat -o")
+
+print("\n---test multiple output files---")
+command("./copycat -o xout.txt -o xout2.txt")
+
+print("\n--test invalid output files---")
 command("chmod 000 xout.txt")
 command("./copycat -o xout.txt lipsum.txt")
 command("chmod 777 xout.txt")
-print("---test invalid input files---")
+
+print("\n---test invalid input files---")
 command("./copycat -o xout.txt nonexistent file")
-print("---test multiple file concatenation---")
+
+print("\n---test multiple file concatenation---")
 command("split -n 10 lipsum.txt")
 command("./copycat -o xout.txt xa*")
 command("diff xout.txt lipsum.txt")
-print("---test speed for multiple buffer sizes---")
+
+print("\n---test speed for multiple buffer sizes---")
 for i in range(0, 19):
 	buffersize = str(pow(2,i))
 	print("buffersize = " + str(buffersize) + " bytes")
