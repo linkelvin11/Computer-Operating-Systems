bad50085d3b6124d505deb2acdc879103a4270b3
linkelvin11 <lin.kelvin11@gmail.com>
2015-09-21 20:36:09 -0400

    ps2 fixed filepath errors, now working with only absolute paths

--- PS2_Recursive_Filesystem_Exploration/PS2.c
+++ PS2_Recursive_Filesystem_Exploration/PS2.c
@@ -16,58 +16,60 @@
 #include <string.h>
 #include <limits.h>
 
-char path[128];
+int rootlen;
 
-int lsDir(DIR *dirp)
+//int lsDir(DIR *dirp)
+int lsDir(char *path)
 {
-	if (dirp == NULL)
+	struct dirent *tdir;
+	DIR *tdirp, *odirp;
+	char cPath[256];
+	tdirp = opendir(path);
+	if (tdirp == NULL)
 	{
 		perror("Cannot list NULL pointer\n");
 		return -1;
 	}
-	//struct dirent *tdir = malloc(sizeof (struct dirent));
-	struct dirent *tdir;
 	int i;
 	for(i = 0; i < 2; i++)
 	{
-		if ( (tdir = readdir(dirp)) == NULL)
+		if ( (tdir = readdir(tdirp)) == NULL)
 		{
 			perror("Could not open file\n");
 			return -1;
-		}
+		} 
 	}
-	while( (tdir = readdir(dirp)) )
+	while( (tdir = readdir(tdirp)) )
 	{
 		/*
 		printf("the current d_type is: %c\n",tdir->d_type);
 		printf("DT_DIR = %c \nDT_REG = %c \nDT_BLK = %c \nDT_CHR = %c \n",
 				DT_DIR,DT_REG,DT_BLK,DT_CHR);
 				*/
-		realpath(tdir->d_name,path);
+		sprintf(cPath,"%s%s%s",path,"/",tdir->d_name);
 		switch(tdir->d_type)
 		{
 			case DT_DIR:
 				// print stuff about current tdir
-				printf("Now listing: %s\n",path);
-				if (opendir(path) == NULL)
+				printf("%s\n",cPath+rootlen);
+				odirp = opendir(cPath);
+				if (odirp == NULL)
 				{
-					fprintf(stderr,"could not open directory %s: %s\n",path,strerror(errno));
-					//if(tdir) free(tdir);
+					fprintf(stderr,"could not open directory %s: %s\n",cPath+rootlen,strerror(errno));
 					return -1;
 				}
-				if ((lsDir(opendir(path))) == -1)
+				if ((lsDir(cPath)) == -1)
 				{
 					fprintf
 						(stderr,"could not open directory %s for listing: %s\n",
 							tdir->d_name,
 								strerror(errno));
-					//if(tdir) free(tdir);
 					return -1;
 				}
 				break;
 			case DT_REG:
 				// print stuff about current file
-				printf("found file: %s\n",tdir->d_name);
+				printf("%s/%s\n",cPath+rootlen,tdir->d_name);
 				break;
 			case DT_BLK:
 				break;
@@ -83,15 +85,18 @@ int lsDir(DIR *dirp)
 
 int main(int argc, char **argv)
 {
-	DIR *tdir;
+	char rootdir[256];
+	char tmp[256];
 	if (argc == 1)
-		tdir = opendir("."); // list current directory
-	else if ((tdir = opendir(argv[1])) == NULL)
 	{
-		perror("could not open directory");
-		return -1; // error report here
+		strcpy(tmp,".");
 	}
-	printf("running lsDir on %s\n",argv[1]);
-	realpath(argv[1],path);
-	lsDir(tdir);
+	else{
+		strcpy(tmp,argv[1]);
+	}
+	//printf("running lsDir on %s\n",tdir->d_name);
+	realpath(tmp,rootdir);
+	rootlen = strlen(rootdir)+1;
+	printf("The root path is: \n%s\n",rootdir);
+	lsDir(rootdir);
 }
