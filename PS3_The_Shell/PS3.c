f4de265e22b44f711ab31d0dc27b8f382be8e5c4
linkelvin11 <lin.kelvin11@gmail.com>
2015-09-27 15:57:48 -0400

    ps3 basic io and fork

--- PS3_The_Shell/PS3.c
+++ PS3_The_Shell/PS3.c
@@ -7,9 +7,33 @@
 */
 
 #include <stdio.h>      // error reporting
+#include <stdlib.h>     // exit
+#include <string.h>     // strtok
+
+void eggzeck(char *command)
+{
+    
+    exit(0);    // kill the child process
+}
+
 
 int main(int argc, char **argv)
 {
-   
+    char command[256];
+    int pid = 0;
+    while(1)    // start shell
+    {
+        printf("$: ");
+        scanf("%s",command);
+        pid = fork();
+        if (pid)
+        {
+            printf("i am a parent of %d. Waiting...\n",pid);
+            wait(1);
+            continue;
+        }
+        eggzeck(command);
+    }
+
     return 0;
 }
