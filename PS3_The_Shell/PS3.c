af04bf3ef9a55910111cf1e616c410a1ef5580a8
linkelvin11 <lin.kelvin11@gmail.com>
2015-09-28 20:48:29 -0400

    ps3 basic working shell

--- PS3_The_Shell/PS3.c
+++ PS3_The_Shell/PS3.c
@@ -11,34 +11,45 @@
 #include <string.h>     // strtok
 #include <sys/resource.h> // getrusage()
 #include <sys/wait.h>   // waitpid
+#include <unistd.h>     // exec
 
 void eggzeck(char *command)
 {
-    int i;
-    printf("closing files\n");
+    int i = 1;
+    char** prog = malloc(256*sizeof(char));
+    prog[255] = NULL;
+    char delim[10] = " \t\n";
     fcloseall();
+
+    for (prog[0] = strtok(command,delim); (prog[i] = strtok(NULL,delim)) != NULL && i < 255; i++)
+        
+    execvp(prog[0],prog);
+    free(prog);
     exit(0);    // kill the child process
 }
 
 int main(int argc, char **argv)
 {
-    char command[256];
+    size_t bsize = 256;
+    char command[bsize];
+    char *cmd = command;
     int pid = 0;
     int stat_loc;
+    int i = 0;
     struct rusage status;
     while(1)    // start shell
     {
         printf("$: ");
-        scanf("%s",command);
+        getline(&cmd,&bsize,stdin);
         pid = fork();
         if (pid)
         {
             printf("i am a parent of %d. Waiting...\n",pid);
             waitpid(pid,&stat_loc,0);
+            printf("i am done waiting. Ready for next input\n");
             continue;
         }
         eggzeck(command);
     }
-
     return 0;
 }
