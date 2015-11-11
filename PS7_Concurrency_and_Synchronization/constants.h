8400a44d894ef62186d9ab09d08b9a36d74deda3
linkelvin11 <lin.kelvin11@gmail.com>
2015-11-10 19:38:18 -0500

    ps7 constants.h

---
+++ PS7_Concurrency_and_Synchronization/constants.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <signal.h>
+#include <errno.h>
+#include <string.h>
+
+#define NUMPROC 64
+#define NUMLOOP 1e3
